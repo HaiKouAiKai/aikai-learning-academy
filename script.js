2fd6817533fbdc3617c942dca0ceaefa615afd0b
HaiKouAiKai <haikouaikai@gmail.com>
2026-07-10 11:25:12 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 window.addEventListener("load", () => {
+
     document.body.classList.add("loaded");
+
+    setTimeout(()=>{
+        document.getElementById("logo").classList.add("show");
+    },400);
+
+    setTimeout(()=>{
+        document.getElementById("title").classList.add("show");
+    },1000);
+
+    setTimeout(()=>{
+        document.getElementById("tagline").classList.add("show");
+    },1600);
+
+    setTimeout(()=>{
+        document.getElementById("notice").classList.add("show");
+    },2300);
+
 });
